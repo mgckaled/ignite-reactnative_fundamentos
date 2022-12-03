1eb11e8e3eeeb7dc066da62e60e562bf7356fb87
mgckaled <marcelgckaled@gmail.com>
2022-12-03 12:33:31 -0300

    feat: Use StyleSheet to create style

--- App.tsx
+++ App.tsx
@@ -3,13 +3,27 @@ import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
 	return (
-		<>
-			<Text>React Native</Text>
-			<Text>React Native</Text>
-		</>
+		<View style={styles.container}>
+			<Text style={styles.eventName}>Nome do Evento</Text>
+			<Text style={styles.eventDate}>Sábado, 3 de Dezembro de 2022</Text>
+		</View>
 	)
 }
 
 const styles = StyleSheet.create({
-	container: {}
+	container: {
+		flex: 1,
+		backgroundColor: '#131016',
+		padding: 24
+	},
+	eventName: {
+		color: '#FDFCFE',
+		fontSize: 24,
+		fontWeight: 'bold',
+		marginTop: 48
+	},
+	eventDate: {
+		color: '#6B6B6B',
+		fontSize: 16
+	}
 })
