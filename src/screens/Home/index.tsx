c3e2e440463924462487a480fef640d919bdbe69
mgckaled <marcelgckaled@gmail.com>
2022-12-05 04:39:12 -0300

    feat: add input data

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,19 @@
-import { Text, View } from 'react-native'
+import { Text, TextInput, View } from 'react-native'
 
 import { styles } from './style'
 
-export default function Home() {
+export function Home() {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.eventName}>Nome do evento</Text>
 
 			<Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+
+			<TextInput
+				style={styles.input}
+				placeholder="Nome do participante"
+				placeholderTextColor="#6B6B6B"
+			/>
 		</View>
 	)
 }
