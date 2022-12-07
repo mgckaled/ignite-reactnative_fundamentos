1dc82b29bb3ed5a645a550cf80c01f02c7cc336a
mgckaled <marcelgckaled@gmail.com>
2022-12-07 08:05:09 -0300

    feat: save participant using a state

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -13,17 +13,19 @@ import { Participant } from '../../components/Participant'
 import { styles } from './styles'
 
 export function Home() {
-	const [participants, setParticipants] = useState(['Lucas'])
+	const [participants, setParticipants] = useState<string[]>([])
+	const [participantName, setParticipantName] = useState('')
 
 	function handleParticipantAdd() {
-		if (participants.includes('Marcel')) {
+		if (participants.includes(participantName)) {
 			return Alert.alert(
 				'Participante existe',
 				'Já existe um participante na lista com esse nome.'
 			)
 		}
 
-		setParticipants(prevState => [...prevState, 'Ana'])
+		setParticipants(prevState => [...prevState, participantName])
+		setParticipantName('')
 	}
 
 	function handleParticipantRemove(name: string) {
@@ -50,6 +52,8 @@ export function Home() {
 					style={styles.input}
 					placeholder="Nome do participante"
 					placeholderTextColor="#6B6B6B"
+					onChangeText={setParticipantName}
+					value={participantName}
 				/>
 
 				<TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
